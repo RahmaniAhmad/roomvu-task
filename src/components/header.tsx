0161da83c6d3d5117253eb0a63a25f4c99998332
AhmadRahmani <ahmadrahmani1060@gmail.com>
2023-12-23 00:11:51 +0330

    separate file for toggle swich styles

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -14,18 +14,32 @@ const HeaderRow = styled.div`
 `;
 const Header = () => {
   return (
-    <HeaderWrapper>
-      <HeaderRow>
+    <div style={{ padding: "10px" }}>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+          padding: "10px 0 10px 0",
+        }}
+      >
         <ThemeToggle />
         <h4>Roomvu Task</h4>
-      </HeaderRow>
-      <HeaderRow>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+          padding: "10px 0 10px 0",
+        }}
+      >
         <Link style={{ fontSize: "24px", fontWeight: "900" }} href="/">
           overreacted
         </Link>
         <span style={{ fontStyle: "italic" }}>by Ahmad Rahmani</span>
-      </HeaderRow>
-    </HeaderWrapper>
+      </div>
+    </div>
   );
 };
 
--- src/design-system/ui/toggles/toggleSwitch.tsx
+++ src/design-system/ui/toggles/toggleSwitch.tsx
@@ -1,57 +1,20 @@
 import styled from "styled-components";
+import {
+  SwitchButton,
+  SwitchInput,
+  SwitchLabel,
+  SwitchWrapper,
+} from "./toggleSwitch.styled";
 
 interface ToggleSwitchProps {
   isChecked?: boolean;
   handleToggle?: () => void;
 }
+
 export const ToggleSwitch = ({
-  isChecked = false,
+  isChecked = true,
   handleToggle,
 }: ToggleSwitchProps) => {
-  const SwitchWrapper = styled.div`
-    display: flex;
-    align-items: center;
-  `;
-
-  const SwitchInput = styled.input`
-    display: flex;
-    height: 0;
-    width: 0;
-    visibility: hidden;
-  `;
-
-  const SwitchLabel = styled.label`
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    cursor: pointer;
-    width: 60px;
-    height: 26px;
-    border-radius: 100px;
-    border: 2px solid gray;
-    position: relative;
-    transition: background-color 0.2s;
-  `;
-
-  const SwitchButton = styled.span`
-    content: "";
-    position: absolute;
-    left: 2px;
-    width: 16px;
-    height: 16px;
-    border-radius: 45px;
-    transition: 0.2s;
-    background: grey;
-    box-shadow: 0 0 2px 0 rgba(10, 10, 10, 0.29);
-    ${SwitchInput}:checked + ${SwitchLabel} & {
-      left: calc(100% - 4px);
-      transform: translateX(-100%);
-    }
-
-    ${SwitchLabel}:active & {
-      width: 45px;
-    }
-  `;
   return (
     <SwitchWrapper>
       <SwitchInput
